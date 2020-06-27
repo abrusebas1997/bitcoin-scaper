070070ba59152f9ed3413569d6d4b31264cb6672
Jarquevious <Jarquevious@gmail.com>
2020-06-26 20:24:29 -0400

    first commit bitcoin

--- scrape.go
+++ scrape.go
@@ -12,12 +12,21 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	var coin_name string
+	var coin_price string
+	var coin_change string
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// On every a element which has href attribute call callback
+	c.OnHTML("#__layout > div > div.layout__wrp > div.header-zone.layout__header > header > div > div.tickers-desktop.header-desktop__tickers > ul > li:nth-child(1) > a > span.tickers-desktop__coin-cap", func(e *colly.HTMLElement) {
+		coin_name = e.Text
+	})
+
+	c.OnHTML("#__layout > div > div.layout__wrp > div.header-zone.layout__header > header > div > div.tickers-desktop.header-desktop__tickers > ul > li:nth-child(1) > a > span.tickers-desktop__coin-value", func(e *colly.HTMLElement) {
+		coin_price = e.Text
+	})
+
+	c.OnHTML("#__layout > div > div.layout__wrp > div.header-zone.layout__header > header > div > div.tickers-desktop.header-desktop__tickers > ul > li:nth-child(1) > a > span.tickers-desktop__coin-diff.tickers-desktop__coin-value_down", func(e *colly.HTMLElement) {
+		coin_change = e.Text
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +35,7 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://cointelegraph.com/tags/bitcoin")
+
+	fmt.Println(coin_name, coin_price, coin_change)
 }
